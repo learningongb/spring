4b0f25b89bf633d837df105a8fd3e85960007e6d
learningongb <mnetckachev@yandex.ru>
2024-05-30 22:39:55 +0300

    Сделано на семинаре

---
+++ seminar_3/spring-demo/src/main/java/ru/gb/springdemo/repository/ReaderRepository.java
@@ -0,0 +1,33 @@
+package ru.gb.springdemo.repository;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import ru.gb.springdemo.model.Reader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Repository
+public class ReaderRepository {
+
+  private final List<Reader> readers;
+
+  public ReaderRepository() {
+    this.readers = new ArrayList<>();
+  }
+
+  @PostConstruct
+  public void generateData() {
+    readers.addAll(List.of(
+      new ru.gb.springdemo.model.Reader("Игорь")
+    ));
+  }
+
+  public Reader getReaderById(long id) {
+    return readers.stream().filter(it -> Objects.equals(it.getId(), id))
+      .findFirst()
+      .orElse(null);
+  }
+
+}
---
+++ seminar_3/spring-demo/src/main/java/ru/gb/springdemo/service/IssuerService.java
@@ -0,0 +1,36 @@
+package ru.gb.springdemo.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.gb.springdemo.api.IssueRequest;
+import ru.gb.springdemo.model.Issue;
+import ru.gb.springdemo.repository.BookRepository;
+import ru.gb.springdemo.repository.IssueRepository;
+import ru.gb.springdemo.repository.ReaderRepository;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class IssuerService {
+
+  // спринг это все заинжектит
+  private final BookRepository bookRepository;
+  private final ReaderRepository readerRepository;
+  private final IssueRepository issueRepository;
+
+  public Issue issue(IssueRequest request) {
+    if (bookRepository.getBookById(request.getBookId()) == null) {
+      throw new NoSuchElementException("Не найдена книга с идентификатором \"" + request.getBookId() + "\"");
+    }
+    if (readerRepository.getReaderById(request.getReaderId()) == null) {
+      throw new NoSuchElementException("Не найден читатель с идентификатором \"" + request.getReaderId() + "\"");
+    }
+    // можно проверить, что у читателя нет книг на руках (или его лимит не превышает в Х книг)
+
+    Issue issue = new Issue(request.getBookId(), request.getReaderId());
+    issueRepository.save(issue);
+    return issue;
+  }
+
+}
