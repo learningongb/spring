c9d5074121ed9da7c02d813834f6440b44a70e91
learningongb <mnetckachev@yandex.ru>
2024-06-14 00:09:07 +0300

    Добавил пользователей и роли. Сделал страницу авторизации. Настроил права доступа к страницам

--- seminar_3/spring-demo/src/main/java/ru/gb/springdemo/security/LocalUserDetailsService.java
+++ seminar_3/spring-demo/src/main/java/ru/gb/springdemo/security/LocalUserDetailsService.java
@@ -1,17 +1,14 @@
 package ru.gb.springdemo.security;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-import ru.gb.springdemo.model.Role;
 import ru.gb.springdemo.model.User;
 import ru.gb.springdemo.repository.UserRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class LocalUserDetailsService implements UserDetailsService {
@@ -27,11 +24,9 @@ public class LocalUserDetailsService implements UserDetailsService {
         User user = repository.getUserByName(username).stream().findFirst().orElse(null);
         if (user == null) throw new UsernameNotFoundException(username + " not found");
 
-        List<SimpleGrantedAuthority> l = new ArrayList<>();
-        for (Role role: user.getRoles()) {
-            l.add(new SimpleGrantedAuthority(role.getName()));
-        }
         return new org.springframework.security.core.userdetails.User(user.getName(), user.getPasswordHash(),
-                    l);
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName()))
+                        .collect(Collectors.toList()));
     }
 }
